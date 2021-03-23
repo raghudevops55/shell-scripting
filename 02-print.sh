4d544eaab520875a0b882886a0fda7e33dd6e03c
KalluruS <>
2021-03-23 08:35:00 +0530

    Update shell-scripting/

--- 02-print.sh
+++ 02-print.sh
@@ -38,7 +38,7 @@ echo -e "Hello\n\nWorld"
 # Syntax: echo -e  "\e[COLCODEmMESSAGE"
 
 echo -e "\e[31mHello in Red\e[0m"
-echo -e "\e[1;31mHello in Bold Red\[0m"
+echo -e "\e[1;31mHello in Bold Red\e[0m"
 
 echo  "++++++++++++++  We are done with Printing Options ++++++++++++++++++"
 
