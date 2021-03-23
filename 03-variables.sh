cc6ad270d4c0d582e209b66c257ada5de66089a8
KalluruS <>
2021-03-23 08:40:30 +0530

    Update shell-scripting/

---
+++ 03-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Variable is name you give to certain set of data.
+
+## With out variables
+echo Welcome to DevOps training
+echo Training of DevOps lasts for 70 days
+echo DevOps training covers shell scripting & Ansible
+
+## Declaring a variable , Syntax: var=data
+
+COURSE=DEVOPS
+echo Welcome to ${COURSE} training
+echo Training of $COURSE lasts for 70 days
+echo $COURSE training covers shell scripting & Ansible
