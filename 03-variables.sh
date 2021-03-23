f765a80ac502f779976b1d72897c6e1201ebc0e9
KalluruS <>
2021-03-23 08:40:52 +0530

    Update shell-scripting/

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,11 +5,11 @@
 ## With out variables
 echo Welcome to DevOps training
 echo Training of DevOps lasts for 70 days
-echo DevOps training covers shell scripting & Ansible
+echo "DevOps training covers shell scripting & Ansible"
 
 ## Declaring a variable , Syntax: var=data
 
 COURSE=DEVOPS
 echo Welcome to ${COURSE} training
 echo Training of $COURSE lasts for 70 days
-echo $COURSE training covers shell scripting & Ansible
+echo "$COURSE training covers shell scripting & Ansible"
