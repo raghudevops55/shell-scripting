e462418e3b8d2e058b56923f843a8582fe4b2f6b
KalluruS <>
2021-03-23 08:32:28 +0530

    Update shell-scripting/

--- 02-print.sh
+++ 02-print.sh
@@ -19,3 +19,24 @@ echo Welcome to DevOps Training
 echo -e "Hello\nWorld"
 echo -e "Hello\n\nWorld"
 
+# Color based printing
+
+# Colors      Code
+
+# Red         31
+# Green       32
+# Yellow      33
+# Blue        34
+# Magenta     35
+# Cyan        36
+
+# Reset       0
+# Bold        1
+
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+# Syntax: echo -e  "\e[COLCODEmMESSAGE"
+
+echo -e "\e[31mHello in Red"
+echo -e "\e[1;31mHello in Bold Red"
+
