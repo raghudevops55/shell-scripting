29ab4159589519086787ae145ec8eba6ed482aaa
KalluruS <>
2021-03-25 08:43:45 +0530

    Update shell-scripting/

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -2,14 +2,16 @@
 
 echo -e "\e[1;35m [INFO] Installing Nginx\e[0m"
 yum install nginx -y
-if [ $? -ne 0 ]; then
+STAT=$?
+if [ $STAT -ne 0 ]; then
   echo -e "\e[1;31m [ERROR] Nginx Installation is failure\e[0m"
+  echo "EXIT STATUS = $STAT"
   exit 2
 else
   echo -e "\e[1;32m [SUCC] Nginx Installation is successful"
 fi
 
-
+exit
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
