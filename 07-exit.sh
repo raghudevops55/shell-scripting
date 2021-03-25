c83520dae8b64cdc99df3ed257d800e6f0afaf99
KalluruS <>
2021-03-25 08:19:13 +0530

    Update shell-scripting/

---
+++ 07-exit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# exit status is the status of the command executed. Meaning every command after its execution it sends its status by returning exit status.
+# Exit status is a number and can be accessed  by a variable (?), $? to access the variable
+
