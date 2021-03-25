57fa00616a8ff1487c4b339ca8f3ee237065c0b5
KalluruS <>
2021-03-25 08:21:43 +0530

    Update shell-scripting/

--- 07-exit.sh
+++ 07-exit.sh
@@ -3,3 +3,11 @@
 # exit status is the status of the command executed. Meaning every command after its execution it sends its status by returning exit status.
 # Exit status is a number and can be accessed  by a variable (?), $? to access the variable
 
+
+# Exit states ranges from 0-255
+# 0 - success
+# 1-255 - non success / partial success
+
+# In shell scripting this status number we can use to tell that the script execution is a failure by using exit command.
+
+exit ## returns 0 which means success
