8708c0beaff6039d3ee1e2829dbb2f694f0a1ed9
KalluruS <>
2021-03-25 09:05:34 +0530

    Update shell-scripting/

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -10,16 +10,21 @@ PRINT "Download Frontend Component"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 STAT $? "Download Frontend"
 
-
-exit
-
+PRINT "Extract Frontend"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
+STAT $? "Extracting Frontend"
+
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-master README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+PRINT "Update Nginx Configuration"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $? "Updating Nginx Configuration"
+
+PRINT "Start Nginx Service"
 systemctl enable nginx
 systemctl restart nginx
+STAT $? "Restarting Nginx Service"
