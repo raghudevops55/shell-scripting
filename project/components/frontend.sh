0201226b6df6f4f75b9f439e4d3cd905a351f67a
KalluruS <>
2021-03-25 08:41:24 +0530

    Update shell-scripting/

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo -e "\e[1;35m [INFO] Installing Nginx\e[0m"
 yum install nginxccccc -y
 if [ $? -ne 0 ]; then
-  echo -e "\e[1;31mNginx Installation is failure\e[0m"
+  echo -e "\e[1;31m [ERROR] Nginx Installation is failure\e[0m"
   exit 2
+else
+  echo -e "\e[1;32m Nginx Installation is successful"
 fi
 
 
