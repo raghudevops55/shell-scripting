ede707e795a5681dae5d59afb354b19a52f8dc18
KalluruS <>
2021-03-27 08:25:18 +0530

    Update shell-scripting/

--- project/components/catalogue.sh
+++ project/components/catalogue.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo Installing catalogue
+PRINT "Install NodeJS"
+yum install nodejs make gcc-c++ -y
+STAT $? "Installing NodeJS"
+
+PRINT "Create RoboShop Application User"
+useradd roboshop
+STAT $? "Creating Application User"
+
+PRINT "Download Catalogue Component"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+STAT $? "Downloading Catalogue"
+
+PRINT "Extract Catalogue"
+cd /home/roboshop
+rm -rf catalogue && unzip /tmp/catalogue.zip && mv catalogue-main catalogue
+STAT $? "Extracting Catalogue"
+
+PRINT "Download NodeJS dependencies"
+cd /home/roboshop/catalogue
+npm install
+STAT $? "Downloading dependencies"
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
