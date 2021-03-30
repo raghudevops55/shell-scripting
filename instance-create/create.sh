f3c17ca5e7e407711a5595fb10efc722d9d27c87
KalluruS <>
2021-03-30 08:42:52 +0530

    Update shell-scripting/

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -11,7 +11,7 @@ if [ -z "${COMPONENT}" ]; then
 fi
 
 INSTANCE_EXISTS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
-STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=cart  | jq .Reservations[].Instances[].State.Name | xargs)
+STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[].Instances[].State.Name | xargs)
 if [ -z "INSTANCE_EXISTS" -o "$STATE" == "terminated"  ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 else
@@ -19,7 +19,7 @@ else
 fi
 
 
-IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=cart   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
+IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
 
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z0449177GO3AQK4VZZ1H --change-batch file:///tmp/record.json
