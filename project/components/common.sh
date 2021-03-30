356fd389db63b8f17a4c2ba7b0e6cbee6dba8edf
KalluruS <>
2021-03-30 08:40:31 +0530

    Update shell-scripting/

--- project/components/common.sh
+++ project/components/common.sh
@@ -69,11 +69,11 @@ Install_NodeJS_Dependencies() {
 }
 
 Setup_Service() {
+  PRINT "Setup SystemD Service for ${COMPONENT}"
   mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
   sed -i -e 's/MONGO_DNSNAME/mongodb.devopsb55.tk/' /etc/systemd/system/${COMPONENT}.service
-  systemctl daemon-reload
-  systemctl start catalogue
-  systemctl enable catalogue
+  systemctl daemon-reload && systemctl start catalogue && systemctl enable catalogue
+  STAT $? "Starting ${COMPONENT} Service"
 }
 
 NodeJS_Setup() {
