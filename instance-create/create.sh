8ba4c1aae88c2e44d10b005e1d40b2bf5c2b49cc
KalluruS <>
2021-03-30 08:31:20 +0530

    Update shell-scripting/

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -20,3 +20,6 @@ fi
 
 
 IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=cart   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
+
+sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z0449177GO3AQK4VZZ1H --change-batch file:///tmp/record.json
