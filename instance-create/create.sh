4c0d052f567c24483e185005ee30b756484385ec
KalluruS <>
2021-03-30 08:15:04 +0530

    Update shell-scripting/

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -13,6 +13,8 @@ fi
 INSTANCE_EXISTS=$(aws ec2 describe-instances  --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
 if [ -z "INSTANCE_EXISTS" ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+else
+  echo "Instance ${COMPONENT} already exists"
 fi
 
 
