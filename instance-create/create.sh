dcd097825e7fde3549ce9adb21ccd04b96a0058e
KalluruS <>
2021-03-30 08:20:30 +0530

    Update shell-scripting/

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -10,8 +10,9 @@ if [ -z "${COMPONENT}" ]; then
   exit 1
 fi
 
-INSTANCE_EXISTS=$(aws ec2 describe-instances  --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
-if [ -z "INSTANCE_EXISTS" ]; then
+INSTANCE_EXISTS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
+STATE=$(aws ec2 describe-instances --filters Name=tag:Name,Values=cart  | jq .Reservations[].Instances[].State.Name)
+if [ -z "INSTANCE_EXISTS" -o "$STATE" == "terminated"  ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 else
   echo "Instance ${COMPONENT} already exists"
