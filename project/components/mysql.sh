015fc1622c61e051d51f51e7f256635ec01d9887
KalluruS <>
2021-03-31 08:27:16 +0530

    Update shell-scripting/

--- project/components/mysql.sh
+++ project/components/mysql.sh
@@ -24,11 +24,18 @@ systemctl enable mysqld
 systemctl start mysqld
 STAT $? "Starting MySQL Service"
 
-
-DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log |awk '{print $NF}')
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@123';
+PRINT "Change the Default Password"
+echo show databases | mysql -uroot -pRoboShop@123
+if [ $? -ne 0 ]; then
+  DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log |awk '{print $NF}')
+  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@123';
 uninstall plugin validate_password;" >/tmp/sql
-mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
+  mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
+  STAT $? "Changing MySQL Default Password"
+else
+  PRINT "MySQL Password reset is not required"
+fi
+
 
 
 # curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
