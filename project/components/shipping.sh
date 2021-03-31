712b502d727c3f75988071f10bdbb2d63575d4e5
KalluruS <>
2021-03-31 08:39:05 +0530

    Update shell-scripting/

--- project/components/shipping.sh
+++ project/components/shipping.sh
@@ -5,3 +5,17 @@ COMPONENT=shipping
 
 OS_Prereqs
 
+PRINT "Install Maven"
+yum install maven -y
+STAT $? "Installing Maven"
+
+RoboShop_App_User_Add
+Download_Component_From_GitHub
+Extract_Component
+
+PRINT "Compile Shipping Code"
+cd /home/roboshop/shipping
+mvn clean package && mv target/shipping-1.0.jar shipping.jar
+STAT $? "Compiling Shipping Code"
+
+Setup_Service
