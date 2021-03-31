482a40b182dc5df97d4421d23c66646f335d9106
KalluruS <>
2021-03-31 08:57:14 +0530

    Update shell-scripting/

--- project/components/payment.sh
+++ project/components/payment.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 source components/common.sh
-COMPONENT=redis
+COMPONENT=payment
 
 OS_Prereqs
 
-Install Python 3
-# yum install python36 gcc python3-devel -y
-Create a user for running the application
-# useradd roboshop
-Download the repo.
-$ cd /home/roboshop
-$ curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
-$ unzip /tmp/payment.zip
-$ mv payment-main payment
-Install the dependencies
-# cd /home/roboshop/payment
-# pip3 install -r requirements.txt
-Note: Above command may fail with permission denied, So run as root user
+PRINT "Install Python 3"
+yum install python36 gcc python3-devel -y
+STAT $? "Installing Python"
 
-Update the roboshop user and group id in payment.ini file.
+RoboShop_App_User_Add
+Download_Component_From_GitHub
+Extract_Component
 
-Setup the service
+
+PRINT "Install Python App dependencies"
+cd /home/roboshop/payment
+pip3 install -r requirements.txt
+STAT $? "Installing Dependencies"
+
+#Update the roboshop user and group id in payment.ini file.
+
+
 
 # mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
 # systemctl daemon-reload
