88a48ff1e077093d5f7185978426349090eaf3d2
KalluruS <>
2021-03-31 08:21:05 +0530

    Update shell-scripting/

--- project/components/mysql.sh
+++ project/components/mysql.sh
@@ -28,7 +28,7 @@ STAT $? "Starting MySQL Service"
 DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log |awk '{print $NF}')
 echo "uninstall plugin validate_password;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';" >/tmp/sql
-mysql -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
+mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
 
 
 #> uninstall plugin validate_password;
