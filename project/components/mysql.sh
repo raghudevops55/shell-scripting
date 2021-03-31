518a6115108299c769bbd56c4eb259de5c8d4272
KalluruS <>
2021-03-31 08:20:35 +0530

    Update shell-scripting/

--- project/components/mysql.sh
+++ project/components/mysql.sh
@@ -25,11 +25,10 @@ systemctl start mysqld
 STAT $? "Starting MySQL Service"
 
 
-grep temp /var/log/mysqld.log
-
-# mysql_secure_installation
-
-# mysql -u root -p
+DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log |awk '{print $NF}')
+echo "uninstall plugin validate_password;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';" >/tmp/sql
+mysql -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
 
 
 #> uninstall plugin validate_password;
