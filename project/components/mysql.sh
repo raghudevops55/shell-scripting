24c39861add327d03675d42a3fdde650a33e5b6f
KalluruS <>
2021-03-31 08:23:46 +0530

    Update shell-scripting/

--- project/components/mysql.sh
+++ project/components/mysql.sh
@@ -27,13 +27,9 @@ STAT $? "Starting MySQL Service"
 
 DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log |awk '{print $NF}')
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@123';
-uninstall plugin validate_password;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';" >/tmp/sql
+uninstall plugin validate_password;" >/tmp/sql
 mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
 
-
-#> uninstall plugin validate_password;
-#> ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
 
 # curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
 
