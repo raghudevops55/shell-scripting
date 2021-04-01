d29e924b025ad0cf2577e43ba95211856875ac4b
KalluruS <>
2021-04-01 07:40:56 +0530

    Update shell-scripting/

---
+++ loops.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Shell has two loops fundamentally
+# while and for loop. Two more loops are also there which are inverse to the logic of while and for which are until and select.
+
+## If you want to run a loop based on some condition then while or until loop
+
+## If you want to run a loop based on the inputs we use for and select loop
+
+i=10
+while [ $i -gt 0 ]; do
+  echo "Welcome to DevOps"
+  i=$(($i-1))
+done
+
